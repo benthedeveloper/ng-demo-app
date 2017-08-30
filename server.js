36e746433f659c41b4f8bde91074cf4e80981cc2
Ben Harrison <benh@isoftstone.com>
2017-08-30 13:15:32 -0700

    Updating server.js and setting port to 4200 again

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
+'use strict';
+//Get environment variables
+require('dotenv').config();
+
+// Get dependencies
 const express = require('express');
 const path = require('path');
+const http = require('http');
+//const proxy = require('express-http-proxy');
+const port = process.env.PORT;
 
-const root = './public';
-const public = process.env.PUBLIC || `${root}`;
 const app = express();
 
-app.use(express.static(public));
-console.log(`serving ${public}`);
+//set up proxy for request to server
+// const apiProxy = proxy(process.env.PROXY_URL, {
+//   proxyReqPathResolver: function (req, res) {
+//     return require('url').parse(req.originalUrl).path;
+//   }
+// });
+
+// Proxy all request to /api to server using apiProxy options
+//app.use("/api/*", apiProxy);
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+// Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(`index.html`, { root: root });
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || '3000';
-app.listen(port, () => console.log(`API running on localhost:${port}`));
+/**
+ * Get port from environment and store in Express.
+ */
+app.set('port', port);
+
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`App running on ${port}, serving from /dist folder`));
