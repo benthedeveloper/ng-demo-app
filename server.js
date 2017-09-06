75a341c69dd65365f465c8a496038b5122313071
Ben Harrison <benh@isoftstone.com>
2017-09-06 15:01:27 -0700

    try adding proxy for /api routes

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const http = require('http');
-//const proxy = require('express-http-proxy');
+const proxy = require('express-http-proxy');
 const port = process.env.PORT;
 
 const app = express();
 
 //set up proxy for request to server
-// const apiProxy = proxy(process.env.PROXY_URL, {
-//   proxyReqPathResolver: function (req, res) {
-//     return require('url').parse(req.originalUrl).path;
-//   }
-// });
+const apiProxy = proxy(process.env.PROXY_URL, {
+  proxyReqPathResolver: function (req, res) {
+    return require('url').parse(req.originalUrl).path;
+  }
+});
 
 // Proxy all request to /api to server using apiProxy options
-//app.use("/api/*", apiProxy);
+app.use("/api/*", apiProxy);
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -44,4 +44,7 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`App running on ${port}, serving from /dist folder`));
+server.listen(port, () => {
+  console.log(`App running on ${port}, serving from /dist folder`);
+  console.log('process.env.PROXY_URL: ', process.env.PROXY_URL);
+});
